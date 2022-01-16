7a8a799e24dda24963fa2df6e05a8666f842a04f
mpramos <mpramos78915400@gmail.com>
2022-01-16 15:41:15 -0400

    en OnChange recibir la funcion de onSearchValueChange usando el useState

--- my-app/src/TodoSearch.js
+++ my-app/src/TodoSearch.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './TodoSearch.css'
 const TodoSearch = () => {
-    const [searchValue, setSearchValue]=React.useState("search");
+    const [searchValue, setSearchValue]=React.useState("");
+    const onSearchValueChange =(event)=>{
+        console.log(event.target.value)
+        setSearchValue(event.target.value)
+    }
     return [
         <input 
         className="TodoSearch"
          placeholder="Cebolla"
-         onChange ={()=>setSearchValue("searchValue")}
+         onChange = {onSearchValueChange}
          />,
          <p>{searchValue}</p>
     ];
 
 };
-export {TodoSearch};
+export {TodoSearch}; 
