c0e02a3a4499c0064676a55ee753bc46be15df90
mpramos <mpramos78915400@gmail.com>
2022-03-31 11:15:24 -0400

    Se agregó una libreria de fuentes en index.html

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import './TodoCounter.css';
-const estilos = {
-  color: 'red',
-  backgroundColor:'yellow'
-}
+
 export default function TodoCounter() {
   return (
-    <h2 className="TodoCounter" style={estilos}>Has completado 2 de 3 TODOs</h2>
+    <h2 className="TodoCounter" >Has completado 2 de 3 TODOs</h2>
   );
 
 }
