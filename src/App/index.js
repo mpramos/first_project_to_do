111975d07b8337c6bd37f83e1da64e4789e5e7ab
mpramos <mpramos78915400@gmail.com>
2022-04-06 09:15:28 -0400

    Persistencia de datos

--- src/App/index.js
+++ src/App/index.js
@@ -9,7 +9,15 @@ const defaultTodos = [
 ];
 
 function App(props) {
-  const [todos,setTodos]=React.useState(defaultTodos)
+  let parsedTodos;
+  const localStorageTodos= localStorage.getItem('TODO_V1')
+  if(!localStorageTodos){
+    localStorage.setItem('TODO_V1', JSON.stringify([]))
+    parsedTodos=[]
+  }else{
+    parsedTodos=JSON.parse(localStorageTodos)
+  }
+  const [todos,setTodos]=React.useState(parsedTodos)
   const [searchValue, setSearchValue]= React.useState('')
   const todosCompleted= todos.filter(todo=>!!todo.completed).length;
   const todoTotal= todos.length;
@@ -23,6 +31,11 @@ function App(props) {
        return todoText.includes(searchText)
     });
   }
+  const saveTodos=(newTodos)=>{
+    const stringifyTodos=JSON.stringify(newTodos)
+    localStorage.setItem('TODO_V1',stringifyTodos);
+    setTodos(newTodos)
+  }
   const completeTodos=(text)=>{
     const todoIndex= todos.findIndex(todo=>todo.text===text);
     let newTodos=[...todos];
@@ -30,13 +43,13 @@ function App(props) {
       text:todos[todoIndex].text,
       completed:true
     }
-    setTodos(newTodos)
+    saveTodos(newTodos)
   }
   const deleteTodos=(text)=>{
       const todoIndex= todos.findIndex(todo=> todo.text===text)
       let newTodos=[...todos];
       newTodos.splice(todoIndex,1)
-      setTodos(newTodos)
+      saveTodos(newTodos)
   }
   return (
     <AppUI
